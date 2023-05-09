0606839646b9ccf20e1c53a65ae45e1debaff3d8
Manuel Valle <mvallet91@gmail.com>
2023-05-09 11:07:42 +0200

    Added parallel categories

--- app.py
+++ app.py
@@ -42,7 +42,15 @@ app.layout = html.Div([
         value=df.columns.values[0],
         labelStyle={'display': 'inline-block'}
     ),
-    dcc.Graph(id='bar-chart')
+    dcc.Graph(id='bar-chart'),
+
+    dcc.Checklist(
+        id='sankey-selector',
+        options=[{'label': c, 'value': c} for c in df.columns],
+        value=['Task', 'Purpose'],
+        labelStyle={'display': 'inline-block'}
+    ),
+    dcc.Graph(id='sankey')
 ])
 
 
@@ -70,6 +78,43 @@ def update_bar_chart(selected_column):
 
     return fig
 
+
+@app.callback(
+    Output('sankey', 'figure'),
+    Input('sankey-selector', 'value'))
+def update_sankey_chart(selected_columns):
+    dimensions = []
+    for col in selected_columns:
+        dimensions.append({
+            'label': col,
+            'values': df[col]
+        })
+
+    fig = go.Figure(go.Parcats(
+        dimensions=dimensions
+    ))
+
+    title = f'{selected_columns} Relationship'
+    fig.update_layout(title_text=title, font_size=10)
+    return fig
+
+
+@app.callback(
+    Output("sankey-selector", "options"),
+    Input("sankey-selector", "value"),
+)
+def update_multi_options(value):
+    options = [{'label': c, 'value': c} for c in df.columns]
+    if len(value) >= 3:
+        options = [
+            {
+                "label": option["label"],
+                "value": option["value"],
+                "disabled": option["value"] not in value,
+            }
+            for option in options
+        ]
+    return options
 
 
 # Run the app
